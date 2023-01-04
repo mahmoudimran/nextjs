cba500d472011b9be0ef91e37cf30dce6ad7155e
mahmoudimran <117694606+mahmoudimran@users.noreply.github.com>
2023-01-04 08:56:34 +0200

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <p>
-        2022 Fine Store Headphones All rights reserved
+        2023 Fine Store Headphones All rights reserved
       </p>
       <p className="icons">
         <AiFillInstagram />
